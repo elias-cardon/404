e9ac92e6a2683ee3081276389037f62c7d3a20c0
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-10-07 13:02:01 +0200

    On fait un coming soon

--- 404.php
+++ 404.php
@@ -5,18 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404</title>
+    <title>Coming soon</title>
 	<link rel="shortcut icon" type="image/x-icon" href="LOGO-ENCART.png" />
 	<link rel="stylesheet" href="404.css">
 </head>
 <body>
 <div id="container">
     <div class="content">
-        <h2>404</h2>
-        <h4>Oups ! La page est introuvable</h4>
-        <p>La page que vous cherchez n'existe pas. Vous avez peut-être mal écrit l'adresse ou la page a peut-être été
-        supprimé.</p>
-        <a href="index.php">Retour vers l'acceuil</a>
+        <h2>Coming soon</h2>
+        <h4>Ne vous inquiétez pas !</h4>
+        <p>Le site est en cours de construction. Il est bientôt finit. Vous pouvez bientôt gâter vos amis à poils</p>
     </div>
 </div>
 <script src="404.js"></script>
